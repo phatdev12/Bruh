0796defa74eb4a0482da82cd5a1a88fc8c840580
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-03 21:59:54 +0700

    chinh sua 1 ti

--- keep_alive.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.use(express.static('public'));
-app.listen(port, () => console.log(`Bot running on http://localhost:${port}`));
