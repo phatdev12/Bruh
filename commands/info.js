20c9102cb1a8741c8a59279f60a1a0d561732774
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-04 09:45:44 +0700

    update

--- commands/info.js
+++ commands/info.js
@@ -6,8 +6,7 @@ module.exports = {
     const info = new MessageEmbed()
     .setAuthor('| Bruhh Bot' ,'https://media.discordapp.net/attachments/848893653857468417/850944364960022568/Artboard_1.png?width=434&height=434')
     .setThumbnail('https://media.discordapp.net/attachments/848893653857468417/850944364960022568/Artboard_1.png?width=434&height=434')
-    .setImage('https://media.discordapp.net/attachments/848893653857468417/850984125418438666/bannerArtboard_1.png?width=1025&height=355')
-    .setDescription('This bot is created and developed by @❰𝓐.𝓒❱ | Python.exe ⚑#8143, anhmydaika123#4445.\n Please use `br!help` to open help list !. Launched on April 10, 2021\n **Bruhh : Open the world of music !** \n------------------------------------------\n **Support :** https://discord.gg/McDhZpDDZM\n **Website :** https://bruhhbot.000webhostapp.com/')
+    .setDescription('This bot is created and developed by @Thieu Nang Tri Tue#9999, anhmydaika123#4445.\n Please use `br!help` to open help list !. Launched on April 10, 2021\n **Bruhh : Open the world of music !** \n------------------------------------------\n **Support :** https://discord.gg/McDhZpDDZM\n **Top.gg :** https://top.gg/bot/844614150591479848')
     message.channel.send(info)
   }
 }
