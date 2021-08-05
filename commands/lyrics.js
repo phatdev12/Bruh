1ae0cec305c1d69377257ef3e1feb3e96c854a3e
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-05 10:45:09 +0700

    update

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -17,10 +17,11 @@ module.exports = {
     const reactionFilter = (reaction, user) => ['⬅️', '➡️'].includes(reaction.emoji.name) && (message.author.id === user.id)
 
     message.channel.send("Please enter the song name now");
-    await message.channel.awaitMessages(messageFilter, { max: 1, time: 15000 }).then(async collected => {
-        songName = collected.first().content;
-        await finder (artist, songName, message, pages)
-    })
+      await message.channel.awaitMessages(messageFilter, { max: 1, time: 15000 }).then(async collected => {
+          songName = collected.first().content;
+          await finder (artist, songName, message, pages)
+      })
+    songName = collected.first().content;
 
     const lyricEmbed = await message.channel.send(`Lyrics page: ${currentPage+1}/${pages.length}`, pages[currentPage])
     await lyricEmbed.react('⬅️');
