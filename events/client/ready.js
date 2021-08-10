bcf6b3951043324e7c7df0fbcd6f73ed93961c02
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-10 15:31:15 +0700

    update

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,13 +1,17 @@
-module.exports = (client) => {
-    console.log('Is online')
-    const serverIn = client.guilds.cache.size;
-    client.user.setPresence({
-        activity: {
-            name: `${serverIn} Servers | br!help`,
-            image: "https://media.giphy.com/media/5Ztn33chuvutW/giphy.gif",
-            type: 'LISTENING',
-            buttons: [{label: "My Website", url: "https://bruhhbot.000webhostapp.com/"}]
-        },
-        status: 'LISTENING'
-    })
+module.exports = {
+    name: 'ready',
+    once: true,
+    execute(client){
+        console.log('Is online')
+        const serverIn = client.guilds.cache.size;
+        client.user.setPresence({
+            activity: {
+                name: `${serverIn} Servers | br!help`,
+                image: "https://media.giphy.com/media/5Ztn33chuvutW/giphy.gif",
+                type: 'LISTENING',
+                buttons: [{label: "My Website", url: "https://bruhhbot.000webhostapp.com/"}]
+            },
+            status: 'LISTENING'
+        })
+    }
 }
--- handlers/events_handler.js
+++ handlers/events_handler.js
@@ -7,7 +7,11 @@ module.exports = (client, Discord) => {
         for(const file of event_files){
             const event = require(`../events/${dir}/${file}`);
             const event_name = file.split('.')[0]
-            client.on(event_name, event.bind(null, client));
+            if (event.once) {
+                client.once(event_name, (...args) => event.execute(...args, client));
+            } else {
+                client.on(event_name, (...args) => event.execute(...args, client));
+            }
         }
     }
     ['client', 'guild'].forEach(e => load_dir(e));
