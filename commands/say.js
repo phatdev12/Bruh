adba9533ac4ca0eb9a666b1784dfdf0a3089623a
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-10 20:50:20 +0700

    updates

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,7 @@
 const { Util,Client, MessageEmbed, Collection } = require('discord.js');
 module.exports = {
   name : 'say',
-  execute(client, message, args){
+  execute(message, args, client){
     let chui = args.slice().join(' ')
     if(message.deletable) message.delete()
     if(!chui) return message.channel.send("Please enter `something`")
