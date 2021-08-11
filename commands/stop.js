98d4d95ac7ebc6ea42b69908e868afe17df7041a
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-11 10:21:27 +0700

    update

---
+++ commands/stop.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'stop',
+    async execute(message, args, client, queue, searcher){
+        const serverQueue = queue.get(message.guild.id)
+        if(!serverQueue)
+                return message.channel.send("There is no music playing!")
+        if(message.member.voice.channel != message.guild.me.voice.channel)
+            return message.channel.send("You need to join the voice chat first!")
+        serverQueue.songs = [];
+        serverQueue.connection.dispatcher.end();
+    }
+}
