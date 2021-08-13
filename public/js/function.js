ec795fcfd565261ba7624725aec9f53e8d8ca643
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-13 22:53:50 +0700

    Update

--- public/js/function.js
+++ public/js/function.js
@@ -11,3 +11,25 @@ span.addEventListener("copy", function(event) {
     console.log(event.clipboardData.getData("text"))
   }
 });
+const help = document.getElementById("help");
+
+help.onclick = function() {
+  document.execCommand("copy");
+}
+
+help.addEventListener("copy", function(event) {
+  event.preventDefault();
+  if (event.clipboardData) {
+    event.clipboardData.setData("text/plain", help.textContent);
+    console.log(event.clipboardData.getData("text"))
+  }
+});
+window.addEventListener("scroll", function() {showFunction()});
+
+function showFunction() {
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+        document.getElementById("card").style.opacity = 1;
+    } else {
+        document.getElementById("card").style.opacity = 0;
+    }
+}
