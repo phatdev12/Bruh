4923df67b453dfcb4c8f8e006f6ff494adf9120e
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-13 14:32:38 +0700

    update

--- keep_alive.js
+++ keep_alive.js
@@ -2,5 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.get('/', () => {
+    app.use(express.static('public'));
+})
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + "./error/404.html")
+})
 app.listen(port, () => console.log(`Bot running on http://localhost:${port}`));
---
+++ public/js/function.js
@@ -0,0 +1,13 @@
+const span = document.getElementById("span");
+
+span.onclick = function() {
+  document.execCommand("copy");
+}
+
+span.addEventListener("copy", function(event) {
+  event.preventDefault();
+  if (event.clipboardData) {
+    event.clipboardData.setData("text/plain", span.textContent);
+    console.log(event.clipboardData.getData("text"))
+  }
+});
