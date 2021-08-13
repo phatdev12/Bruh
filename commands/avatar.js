bafe8225cb5a3319f2bd8e688fe61ee5c083da24
Từ Thắng Phát <81029660+tuthangphat@users.noreply.github.com>
2021-08-13 13:44:09 +0700

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,23 +3,23 @@ module.exports = {
   name: 'avt',
   description: 'avatar',
   async execute(message, client, args){
-    let user = await message.author;
-    let avatar;
-
-    //when args are given, set user to mentioned user
-    if (args.length > 0) {
-        let userID = args[0].slice(3, -1)
-        user = message.guild.members.cache.get(userID).user;
+    const embed = new Discord.MessageEmbed()
+ 
+    if(!message.mentions.users.first()){
+        embed.setTitle("Your Avatar:")
+        embed.setThumbnail(message.author.displayAvatarURL())
+        embed.setDescription("This is your avatar.")
+        embed.setColor("RANDOM")
+        return message.channel.send(embed)
+    }else{
+        const user = message.mentions.users.first()
+        embed.setTitle(`${user.tag}'s Avatar:`)
+        embed.setThumbnail(user.displayAvatarURL())
+        embed.setDescription(`This is ${user.tag}'s avatar.`)
+        embed.setColor('RANDOM')
+        embed.setFooter("This is a test.")
+        return message.channel.send(embed)
     }
-
-    avatar = getUserAvatar(user);
-
-
-    let embed = new MessageEmbed()
-        .setTitle(`Avatar ${user.tag}`)
-        .setImage(avatar)
-        .setColor('RANDOM')
-    message.channel.send(embed)
   }
 }
 //This returns the AvatarURL of the user
