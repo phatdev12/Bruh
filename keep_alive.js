87068442296f847362171133cde027aaea1a7cb9
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-13 15:58:34 +0700

    update

--- keep_alive.js
+++ keep_alive.js
@@ -4,11 +4,4 @@ const port = process.env.PORT || 3000;
 
 
 app.use(express.static('public'));
-
-app.use(function (req,res,next){
-	res.status(404).sendFile('public/404.html');
-});
-app.use(function (req,res,next){
-	res.status(500).sendFile('public/404.html');
-});
 app.listen(port, () => console.log(`Bot running on http://localhost:${port}`));
