de47d9b74dd530770b61dc8504d672f15d45e055
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-14 17:23:17 +0700

    Update

--- public/js/function.js
+++ public/js/function.js
@@ -24,7 +24,7 @@ help.addEventListener("copy", function(event) {
     console.log(event.clipboardData.getData("text"))
   }
 });
-window.addEventListener("scroll", function() {showFunction()});
+window.addEventListener("scroll", function() {showFunction(), showFunction1(), showFunction2(), showFunction3(), showFunction4()});
 
 function showFunction() {
     if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
@@ -32,4 +32,32 @@ function showFunction() {
     } else {
         document.getElementById("card").style.opacity = 0;
     }
+}
+function showFunction2() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      document.getElementById("text_info").style.opacity = 1;
+  } else {
+      document.getElementById("text_info").style.opacity = 0;
+  }
+}
+function showFunction1() {
+  if (document.body.scrollTop > 770 || document.documentElement.scrollTop > 770) {
+      document.getElementById("card1").style.opacity = 1;
+  } else {
+      document.getElementById("card1").style.opacity = 0;
+  }
+}
+function showFunction3() {
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+      document.getElementById("text_info1").style.opacity = 1;
+  } else {
+      document.getElementById("text_info1").style.opacity = 0;
+  }
+}
+function showFunction4() {
+  if (document.body.scrollTop > 1400 || document.documentElement.scrollTop > 1400) {
+      document.getElementById("text_info2").style.opacity = 1;
+  } else {
+      document.getElementById("text_info2").style.opacity = 0;
+  }
 }
