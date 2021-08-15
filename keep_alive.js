8c4a2e216f5fd50515d18ff9e528827a3396597a
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-15 16:25:22 +0700

    update

--- keep_alive.js
+++ keep_alive.js
@@ -4,10 +4,4 @@ const port = process.env.PORT || 3000;
 
 
 app.use(express.static('public'));
-
-app.get('/cookie',function(req,res){
-    res.sendFile(path.join(__dirname+'/Core/cookie.html'));
-});
-
-
 app.listen(port, () => console.log(`Bot running on http://localhost:${port}`));
