a137d39aeaaf91eff1531613fe882a1b3393d998
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-15 17:25:13 +0700

    update

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
 const Discord = require('discord.js');
 const { Util,Client, MessageEmbed, Collection, Intents } = require('discord.js');
 const { executionAsyncResource } = require('async_hooks');
-const client = new Client({
-  allowedMentions: { parse: ['users', 'roles'], repliedUser: true },
-  intents: [
-  Intents.FLAGS.GUILDS, 
-  Intents.FLAGS.GUILD_MEMBERS, 
-  Intents.FLAGS.GUILD_BANS, 
-  Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, 
-  Intents.FLAGS.GUILD_INTEGRATIONS, 
-  Intents.FLAGS.GUILD_WEBHOOKS, 
-  Intents.FLAGS.GUILD_INVITES, 
-  Intents.FLAGS.GUILD_VOICE_STATES, 
-  Intents.FLAGS.GUILD_PRESENCES, 
-  Intents.FLAGS.GUILD_MESSAGES, 
-  Intents.FLAGS.GUILD_MESSAGE_REACTIONS, 
-  Intents.FLAGS.GUILD_MESSAGE_TYPING, 
-  Intents.FLAGS.DIRECT_MESSAGES, 
-  Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-  Intents.FLAGS.DIRECT_MESSAGE_TYPING
-  ]
-});
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const { Player } = require("discord-player");
 const fetch = require('node-fetch');
 const discordButtons = require("discord-buttons-plugin")
