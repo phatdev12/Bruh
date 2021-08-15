0e8934196835dabfc060652f74ed0830fbe34761
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-15 16:14:59 +0700

    update

--- keep_alive.js
+++ keep_alive.js
@@ -4,4 +4,14 @@ const port = process.env.PORT || 3000;
 
 
 app.use(express.static('public'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+app.get('/cookie',function(req,res){
+    res.sendFile(path.join(__dirname+'/Core/cookie.html'));
+});
+
+
 app.listen(port, () => console.log(`Bot running on http://localhost:${port}`));
--- public/js/function.js
+++ public/js/function.js
@@ -61,4 +61,18 @@ function showFunction4() {
       document.getElementById("text_info2").style.opacity = 0;
   }
 }
-
+const cookieBox = document.querySelector(".cookie"),
+acceptBtn = cookieBox.querySelector("button");
+acceptBtn.onclick = ()=>{
+  console.log('clicked')
+  //setting cookie for 1 month, after one month it'll be expired automatically
+  document.cookie = "CookieBy=CodingNepal; max-age="+60*60*24*30;
+  if(document.cookie){ //if cookie is set
+    cookieBox.classList.add("hide"); //hide cookie box
+  }else{ //if cookie not set then alert an error
+    alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
+  }
+}
+let checkCookie = document.cookie.indexOf("CookieBy=CodingNepal"); //checking our cookie
+//if cookie is set then hide the cookie box else show it
+checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
